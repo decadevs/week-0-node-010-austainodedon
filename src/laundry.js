6579c39cd6764ec07a67b1bd1c11becc68bbae21
Decagon <decagon@Decagons-MacBook-Air.local>
2022-02-08 05:28:23 +0100

    revised and completed

--- src/laundry.js
+++ src/laundry.js
@@ -8,58 +8,71 @@
  * @returns {number}
  */
 function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
+  //Procedure, first, get the no of poosible pairs in the clean,
+  //second, given the number of possible washes establish how many
+  //dirty socks should be washed also considering the neededone to pair
+  //the clean sock
+
   // To start Create 2 Objects of Clean and Dirty socks
-  let cleanStorage = {};
-  let dirtyStorage = {};
-  //Loop through checking
+  //storage to keep track of things(no of socks)
+  let cleanContainer = {};
+  let dirtyContainer = {};
+  //Loop through to build the containers from the initial given array
+  //this categorises a sock color to the no pressent {1:3, 2:4}
   for (const cleanSock of cleanPile) {
-    cleanStorage[cleanSock] = cleanStorage[cleanSock] + 1 || 1;
+    //compressed if statement to add one for a color type if seen or to
+    //assign 1 if not seen yet
+    cleanContainer[cleanSock] = cleanContainer[cleanSock] + 1 || 1;
   }
   for (const dirtySock of dirtyPile) {
-    dirtyStorage[dirtySock] = dirtyStorage[dirtySock] + 1 || 1;
+    dirtyContainer[dirtySock] = dirtyContainer[dirtySock] + 1 || 1;
   }
-  // Then Loop through to see if there's clean sock that need to be paired
-  // and check if it's available in the dirty storage
-  for (const sock of Object.keys(dirtyStorage)) {
+  // Then Loop through the dirty container to see if there's clean sock
+  //that need to be paired
+  // and check if it's available in the dirty container considering the no
+  //of washes possible and update the various container and no of whashes remaining
+
+  for (const sock of Object.keys(dirtyContainer)) {
     if (noOfWashes) {
-      if (cleanStorage[sock] % 2 === 1 && dirtyStorage[sock]) {
-        dirtyStorage[sock] -= 1;
-        cleanStorage[sock] += 1;
+      if (cleanContainer[sock] % 2 === 1 && dirtyContainer[sock]) {
+        dirtyContainer[sock] -= 1;
+        cleanContainer[sock] += 1;
         noOfWashes--;
       }
     }
   }
-  // Now Do another loop to check if there's:
-  for (const sock of Object.keys(dirtyStorage)) {
+  // Now Do another loop through the dirty container to check if there are
+  //pairs and still within no of washes
+  for (const sock of Object.keys(dirtyContainer)) {
     // Exact pair of dirty socks
-    if (dirtyStorage[sock] % 2 === 0) {
-      if (noOfWashes > dirtyStorage[sock]) {
-        let washes = dirtyStorage[sock];
-        cleanStorage[sock] = cleanStorage[sock] + washes || washes;
-        dirtyStorage[sock] -= washes;
+    if (dirtyContainer[sock] % 2 === 0) {
+      if (noOfWashes > dirtyContainer[sock]) {
+        let washes = dirtyContainer[sock];
+        cleanContainer[sock] = cleanContainer[sock] + washes || washes;
+        dirtyContainer[sock] -= washes;
         noOfWashes -= washes;
       } else {
         let washes = Math.floor(noOfWashes / 2) * 2;
-        cleanStorage[sock] = cleanStorage[sock] + washes || washes;
-        dirtyStorage[sock] -= washes;
+        cleanContainer[sock] = cleanContainer[sock] + washes || washes;
+        dirtyContainer[sock] -= washes;
         noOfWashes -= washes;
       }
       // Do a pair or more
-    } else if (dirtyStorage[sock] / 2 >= 1) {
-      if (noOfWashes > dirtyStorage[sock]) {
-        let washes = Math.floor(dirtyStorage[sock] / 2) * 2;
-        cleanStorage[sock] = cleanStorage[sock] + washes || washes;
-        dirtyStorage[sock] -= washes;
+    } else if (dirtyContainer[sock] / 2 >= 1) {
+      if (noOfWashes > dirtyContainer[sock]) {
+        let washes = Math.floor(dirtyContainer[sock] / 2) * 2;
+        cleanContainer[sock] = cleanContainer[sock] + washes || washes;
+        dirtyContainer[sock] -= washes;
         noOfWashes -= washes;
       } else {
         let washes = Math.floor(noOfWashes / 2) * 2;
-        cleanStorage[sock] = cleanStorage[sock] + washes || washes;
-        dirtyStorage[sock] -= washes;
+        cleanContainer[sock] = cleanContainer[sock] + washes || washes;
+        dirtyContainer[sock] -= washes;
         noOfWashes -= washes;
       }
     }
   }
-  let pairs = Object.values(cleanStorage);
+  let pairs = Object.values(cleanContainer);
   // Now get the total number of pair by adding up each pairs
   totalPair = pairs.reduce((pairs, sock) => {
     return pairs + Math.floor(sock / 2);
@@ -71,7 +84,5 @@ const noOfWashes = 2;
 const cleanPile = [1, 2, 3, 1, 2, 3];
 const dirtyPile = [3, 3, 4, 1, 2, 7, 9];
 console.log(getMaxPairs(noOfWashes, cleanPile, dirtyPile));
-
-module.exports = getMaxPairs;
 
 module.exports = getMaxPairs;
